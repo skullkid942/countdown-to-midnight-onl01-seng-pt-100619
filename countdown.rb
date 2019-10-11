ce16afd2ccab6b858055cfe8a498071fc390c281
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-11 21:20:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number == 10 
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  number == 5
+  while number > 0 do 
+    sleep 5
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
